963944dd81fe2a787e6c72bb1a2f9f1a47b712e6
evasmidt <shevangeline@gmail.com>
2016-03-21 21:52:13 -0400

    updating columns and adding js functions for calculating

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,35 @@
-var elem = new Foundation.Tabs(element, options);
+/*Restaurant 
+Standard = 15% 
+I'm feeling generous = 20%*/
 
-//_handleTabChange
-$('#element').foundation('_handleTabChange', $target);
+function calc() {
+    var bill = document.getElementByID('bill').value;
+    var tip = bill * .20;
+    var total = bill + tip;
+    document.getElementById("tip").innerHTML = "$" + (tip).toFixed(2);
+    document.getElementById("total").innerHTML = "$" + (total).toFixed(2);    
+}
+
+/*Delivery = 10%
+I'm feeling generous = 15%
+2$ minimum
+
+
+Beauty Salon
+Standard = 15%
+I'm feeling generous = 20%
+
+Massseuse
+Standard =10
+I'm feeling generous = 15
+
+Bartender
+$1 per drink
+Standard = 20%
+I'm feeling generous = 25%
+
+Parking Valet
+$2 - $5
+
+Cab driver
+Standard = 10%*/
